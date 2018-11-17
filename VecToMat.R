3d4a733ef42d66d0c224ff0a3d77e4500beb6c35
Takafumi ITO <itotaka523@gmail.com>
2018-11-17 13:04:44 +0900

    VecToMat.Rの作成．

---
+++ VecToMat.R
@@ -0,0 +1,14 @@
+####数値の入ったベクトルを数値別で01をとる行列へ変換する関数．
+library(tidyverse)
+
+Wide <- function(vec){
+  matA <- matrix(0, length(vec), max(vec))
+  for(i in 1:ncol(matA)){
+    matA[vec == i,i] <- 1
+  }
+  matB <- matA %>% data.frame
+  return(matB)
+}
+
+X <- c(1,2,3,4,5,6,7,8,9,10)
+Wide(X)
